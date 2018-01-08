72839ed61ea5bc400d1879f25508d273306bed65
Armando <arm_antonio_mr@hotmail.com>
2018-01-07 22:28:57 -0600

    Test.java file created

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,9 @@ public class Test{
 
 public static void main(String arg[])
 {
+	Student node = new Student("id1", "Name1", "Tel1");
 	IList <Student>list = new ArrayList<Student>();
+	list.add(node);
 }
 
 }
